fa3fc767e4347ecd9ef9be63e79afdcce520cb08
frontendfocus-764 <sandesha764@gmail.com>
2025-08-27 18:11:15 +0530

    text typing animation

---
+++ year-2025/text-typing-animation/frontendfocus.js
@@ -0,0 +1,14 @@
+   const typed = new Typed("#typed", {
+      strings: [
+        "<span class='color-1'>Developer</span>",
+        "<span class='color-2'>Designer</span>",
+        "<span class='color-3'>Creator</span>",
+        "<span class='color-4'>Innovator</span>"
+      ],
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|"
+    });
