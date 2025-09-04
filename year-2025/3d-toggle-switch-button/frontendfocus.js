34fd5be766a6aefcbb51ce1153ece6253d48cc18
Sandesh Singh <sandesha764@gmail.com>
2025-09-04 20:42:30 +0530

    3d toggle switch button

---
+++ year-2025/3d-toggle-switch-button/frontendfocus.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("light-switch").click();
+    }, 500);
+
+    setTimeout(function () {
+        document.getElementById("light-switch").click();
+    }, 1000);
+});
