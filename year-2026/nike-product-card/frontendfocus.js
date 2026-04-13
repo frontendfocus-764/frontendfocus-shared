b81fd786b01709d204ff7cd0fa4da8f9a16d9bd5
Sandesh Singh <sandesha764@gmail.com>
2026-04-13 11:04:01 +0530

    nike product card

---
+++ year-2026/nike-product-card/frontendfocus.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var modeSwitch = document.querySelector(".mode-switch");
+
+  modeSwitch.addEventListener("click", function () {
+    document.documentElement.classList.toggle("light");
+    modeSwitch.classList.toggle("active");
+  });
+});
