4f28e19ab8864c9f6968b5f26a57facdc72a7a2c
Sandesh Singh <sandesha764@gmail.com>
2026-04-26 17:24:05 +0530

    Delete year-2026/glassmorphism-login-form/main.js js-extra

--- year-2026/glassmorphism-login-form/main.js
+++
@@ -1,9 +0,0 @@
-const passwordInput = document.getElementById("password");
-const togglePasswordBtn = document.getElementById("togglePassword");
-
-togglePasswordBtn.addEventListener("click", () => {
-  const isPassword = passwordInput.type === "password";
-
-  passwordInput.type = isPassword ? "text" : "password";
-  togglePasswordBtn.textContent = isPassword ? "Hide" : "Show";
-});
